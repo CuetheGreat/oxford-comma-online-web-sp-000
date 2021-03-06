357fb0f588d8ba17b77a6f65813ea1a513df0b76
Clayon Stewart <c.stewart89@gmail.com>
2021-03-06 15:02:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,12 @@
 def oxford_comma(array)
   if array.size >= 2
     array[array.size - 1] = "and " + array.last
-    
-  elsearray.join(", ") 
+  end
+  
+  if array.size <= 2
     array.join(" ")
+  else
+    array.join(", ")
   end
   
   
